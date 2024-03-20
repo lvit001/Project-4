8270d97f48c8001e23868bec560a98994579fefc
lvit001 <140283164+lvit001@users.noreply.github.com>
2024-03-19 19:33:13 -0500

    Removed commented out code from files

--- Symptom Checker/static.js
+++ Symptom Checker/static.js
@@ -1,16 +1,3 @@
-// const otherCheckbox = document.querySelector("#other");
-// const otherText = document.querySelector("#otherValue");
-// otherText.style.visibility = "hidden";
-
-// otherCheckbox.addEventListener("change", () => {
-//   if (otherCheckbox.checked) {
-//     otherText.style.visibility = "visible";
-//     otherText.value = "";
-//   } else {
-//     otherText.style.visibility = "hidden";
-//   }
-// });
-
 // Get the container element
 const container = document.getElementById("checkboxContainer");
 
